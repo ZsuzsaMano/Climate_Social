d66307e1d433c8eb07554657b6a10a8e08dbe82c
Zsuzsa.L <manoaway@gmail.com>
2024-01-19 19:43:31 +0100

    style input bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="py-16 text-center">
+    <div className="py-8 text-center">
       <h1 className="text-4xl pb-8">Climate Social</h1>
       <div className="border border-gray-500 mt-2 md:my-3"></div>
     </div>
--- client/src/components/Inputbar.jsx
+++ client/src/components/Inputbar.jsx
@@ -14,20 +14,24 @@ const InputBar = () => {
   };
 
   return (
-    <div>
-      <form action="" className="inputbar">
-        <div className="message">
-          <textarea
-            name="message"
-            id="message"
-            value={comments}
-            placeholder="Message"
-            onChange={handleOnChangeComment}
-          ></textarea>
-          <button onClick={handlePostMsg}>SEND</button>
-        </div>
-      </form>
-    </div>
+    <form className="w-full">
+      <div className="flex flex-col items-center">
+        <textarea
+          name="message"
+          id="message"
+          value={comments}
+          placeholder="Message"
+          onChange={handleOnChangeComment}
+          className="p-2 text-sm w-full md:w-2/3 outline-2 placeholder:text-gray-500 rounded-md shadow-[1px_1px_2px_1px_#a2a2ad]"
+        ></textarea>
+        <button
+          onClick={handlePostMsg}
+          className="w-full md:w-2/3 mt-4 bg-blue-600 text-white active:bg-emerald-600 font-bold uppercase  py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
+        >
+          SEND
+        </button>
+      </div>
+    </form>
   );
 };
 
