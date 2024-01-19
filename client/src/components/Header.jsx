b7615a17d7b14bf96a85a74551862df76b9521fb
Zsuzsa.L <manoaway@gmail.com>
2024-01-19 19:17:27 +0100

    style header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="">
-      <h1 className="">Climate Social</h1>
+    <div className="py-16 text-center">
+      <h1 className="text-4xl pb-8">Climate Social</h1>
+      <div className="border border-gray-500 mt-2 md:my-3"></div>
     </div>
   );
 };
